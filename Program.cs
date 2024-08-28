3afb50b23a2f9c2de27d007c4f2fb99f12a1a7fc
Dinoosauro <80783030+Dinoosauro@users.noreply.github.com>
2024-08-28 12:39:08 +0200

    Skip duplicates with same file name or URL
    - It's now possible to skip the download of a podcast episode:
     * If there's a file with the same name in the output directory
     * If the file has already been downloaded (-> if the URL is saved in the History.txt file)
    - It's also possible to exit the application when the first duplicate is found

--- Program.cs
+++ Program.cs
@@ -62,6 +62,18 @@ internal class Program
                 case "--download-directory":
                     settings.DownloadDirectory = args[i + 1];
                     break;
+                case "--skip-download-if":
+                case "--skip-download":
+                    settings.DuplicateLogic = (Settings.SkipOptions)int.Parse(args[i + 1]);
+                    break;
+                case "--exit-with-first-duplicate":
+                case "--exit-duplicate":
+                case "--exit-duplicates":
+                case "--exit":
+                case "-e":
+                    settings.BreakAtFirstDuplicate = true;
+                    break;
+
 
             }
         }
@@ -106,7 +118,31 @@ internal class Program
                         foreach (PodcastItemContainer container in prompt)
                         {
                             if (container.Title == null || container.URL == null) continue; // Required fields
-                            var Task = ctx.AddTask(container.Title ?? container.URL ?? "", new ProgressTaskSettings { AutoStart = false });
+                            string? FileExtension = GetFileExtensionFromUrl(container.URL);
+                            settings.DownloadDirectory ??= AppDomain.CurrentDomain.BaseDirectory + Path.DirectorySeparatorChar + "PodcastDownloader" + Path.DirectorySeparatorChar + PodcastContainer.Name; // If the user hasn't specified the download directory, we'll use the path of the executable
+                            if (!settings.DownloadDirectory.EndsWith(Path.DirectorySeparatorChar)) settings.DownloadDirectory += Path.DirectorySeparatorChar;
+                            string path = settings.DownloadDirectory + container.Title + FileExtension;
+                            int shouldBreak = -1; // If 0, the download should be skipped. If 1, the application must break.
+                            switch (settings.DuplicateLogic)
+                            {
+                                case Settings.SkipOptions.SKIP_FILE_OVERWRITE: // If a file with the same name exists, skip it
+                                    if (File.Exists(path)) shouldBreak = settings.BreakAtFirstDuplicate ? 1 : 0;
+                                    break;
+                                case Settings.SkipOptions.SKIP_FILE_DOWNLOADED_URL: // If the same URL has been downloaded previously, skip it
+                                    if (History.GetHistoryEntry(container.URL)) shouldBreak = settings.BreakAtFirstDuplicate ? 1 : 0;
+                                    break;
+                            }
+                            if (shouldBreak == 0)
+                            {
+                                AnsiConsole.MarkupLine("[grey]Skipping download of " + container.Title.Replace("[", "[[").Replace("]", "]]") + "[/]");
+                                continue;
+                            }
+                            else if (shouldBreak == 1)
+                            {
+                                AnsiConsole.MarkupLine("[red]Found duplicate with " + container.Title.Replace("[", "[[").Replace("]", "]]") + ". The application will be closed.[/]");
+                                break;
+                            }
+                            var Task = ctx.AddTask((container.Title ?? container.URL ?? "").Replace("[", "[[").Replace("]", "]]"), new ProgressTaskSettings { AutoStart = false });
                             try
                             {
                                 // Download audio file
@@ -114,10 +150,6 @@ internal class Program
                                 response.EnsureSuccessStatusCode();
                                 long? bytes = response.Content.Headers.ContentLength;
                                 using Stream stream = await response.Content.ReadAsStreamAsync();
-                                string? FileExtension = GetFileExtensionFromUrl(container.URL);
-                                settings.DownloadDirectory ??= AppDomain.CurrentDomain.BaseDirectory + Path.DirectorySeparatorChar + "PodcastDownloader" + Path.DirectorySeparatorChar + PodcastContainer.Name; // If the user hasn't specified the download directory, we'll use the path of the executable
-                                if (!settings.DownloadDirectory.EndsWith(Path.DirectorySeparatorChar)) settings.DownloadDirectory += Path.DirectorySeparatorChar;
-                                string path = settings.DownloadDirectory + container.Title + FileExtension;
                                 Directory.CreateDirectory(path[..path.LastIndexOf(Path.DirectorySeparatorChar)]);
                                 using FileStream file = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 8192, true);
                                 byte[] buffer = new byte[8192];
@@ -131,6 +163,7 @@ internal class Program
                                 }
                                 file.Close(); // Close the file so that it can be later read by TagLib-Sharp
                                 file.Dispose();
+                                History.WriteToHistory(container.URL); // Save the entry to the history, so that, if the user wants so, this URL won't be re-downloaded
                                 if (settings.AddMetadata) await Metadata.Apply(path, container, PodcastContainer.AlbumArt, PodcastContainer.Name, PodcastContainer.Category, settings);
                                 Task.Increment(100); // Mark it completed
                                 AnsiConsole.MarkupLine("[grey]Sleeping... [[" + settings.SleepTime + " ms]][/]");
